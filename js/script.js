62d301d24aaf48f7ea4e5cdeabd57473723659a9
amangeldiyew <amangeldiyew.amangeldi@gmail.com>
2025-07-16 00:47:19 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const images = document.querySelectorAll(".slides img");
+let index = 0;
+
+// Set the first image as active
+images[index].classList.add("active");
+
+function fadeToNextSlide() {
+  images[index].classList.remove("active");
+  index = (index + 1) % images.length;
+  images[index].classList.add("active");
+}
+
+setInterval(fadeToNextSlide, 5000);
+
+
+
+
+//Carousel - Services
+document.querySelectorAll('.services-carousel-wrapper').forEach(wrapper => {
+  const carousel = wrapper.querySelector('.services-carousel');
+  const card = wrapper.querySelector('.service-card');
+  const leftArrow = wrapper.querySelector('.arrow.left');
+  const rightArrow = wrapper.querySelector('.arrow.right');
+
+  const cardWidth = card.offsetWidth + 30;
+
+  rightArrow.addEventListener('click', () => {
+    carousel.scrollBy({ left: cardWidth, behavior: 'smooth' });
+  });
+
+  leftArrow.addEventListener('click', () => {
+    carousel.scrollBy({ left: -cardWidth, behavior: 'smooth' });
+  });
+});
